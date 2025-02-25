bf9d1547956f372288ecaf7a5aa808bbbaa57bcd
samvellutini <sam.vellutini@hotmail.com>
2025-02-25 13:20:40 -0800

    problem 16

---
+++ prob16/main.py
@@ -0,0 +1,22 @@
+with open ('prob16/input.txt', 'r') as file:
+    text = file.readlines()
+    for i in range(len(text)):
+        text[i] = (int) (text[i].strip('/n'))
+    print(text)
+    def numerate(j):
+        count = 0
+        stock = text[j]
+        for i in range(len(text)):
+            if text[i] == stock:
+                count+=1
+                text.pop[i]
+        print(count)
+
+
+    trending = 0
+    second_place = 0
+    numerate(1)
+   ## for line in text:
+     #   numerate(line)
+    
+        
